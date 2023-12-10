dc39821b682ac4f36612ac24b4b173e9401d21b0
mfauzann982 <mfauzann982@gmail.com>
2023-12-10 20:54:17 +0700

    fix db connect

--- BackEnd/db/db.js
+++ BackEnd/db/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const db = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    const connect = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
     console.log(error);
