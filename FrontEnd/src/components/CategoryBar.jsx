15a432f1da9919e78342897298e1ef70a827cf2e
mfauzann982 <mfauzann982@gmail.com>
2023-12-15 19:20:01 +0700

    fix category bar

--- FrontEnd/src/components/CategoryBar.jsx
+++ FrontEnd/src/components/CategoryBar.jsx
@@ -11,7 +11,6 @@ const CategoryBar = () => {
   const fetchDataProduct = async () => {
     try {
       const response = await axios.get(baseURLAPI('category'));
-      // console.log(response.data.categorys);
       setAllProducts(response.data.categorys);
     } catch (error) {
       console.error(error);
@@ -45,7 +44,7 @@ const CategoryBar = () => {
             } `}
             key={item.name}
           >
-            <div className="">
+            <div className="whitespace-nowrap">
               <p>{item.name}</p>
             </div>
           </button>
--- FrontEnd/src/pages/homePage.jsx
+++ FrontEnd/src/pages/homePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
     <div className="pb-20 ">
       <div
         className={`fixed w-full max-w-lg top-0 left-0  md:left-1/2 md:-translate-x-1/2 z-10 pb-3 transition-all duration-500 ease-linear ${
-          isFixed ? 'bg-white  shadow-md' : 'bg-bgColorPrimary'
+          isFixed ? 'bg-white shadow-md' : 'bg-bgColorPrimary'
         }`}
       >
         <h1 className="font-bold text-primary text-center p-5 text-2xl">
